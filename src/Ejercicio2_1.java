f6a8caf0615f89e5a8ee0d82bd3f1ea47ae10c5e
Marta-Nich <martaortega2411@gmail.com>
2022-04-29 20:00:11 +0200

    sepia

--- src/Ejercicio2_1.java
+++ src/Ejercicio2_1.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 public class Ejercicio2_1 {
     public static void main(String[] args) throws IOException {
         
-        File f = new File("./archivos/penyagolosa.bmp");
+        File f = new File("resources/penyagolosa.bmp");
         
         TransformaImagen ti = new TransformaImagen(f);
         ti.transformaNegativo();
         ti.transformaOscuro();
         ti.transformaBlancoNegro();
+        ti.transformaSepia();
     }
 }
--- src/TransformaImagen.java
+++ src/TransformaImagen.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 
 public class TransformaImagen {
     File f = null;
@@ -11,12 +12,8 @@ public class TransformaImagen {
         // Control de existencia del fichero y control de la extensión .bmp (sacar
         // mensajes de error)
         try {
-            boolean extension = false;
-            String[] partes = fEnt.getAbsolutePath().split(".");
-            if (partes[1].equals("bmp")) {
-                extension = true;
-            }
-            if (fEnt.exists() && extension) {
+            String file = fEnt.getAbsolutePath();
+            if (fEnt.exists() && file.toLowerCase(Locale.ROOT).endsWith(".bmp")) {
                 f = fEnt;
             }
         } catch (Exception e) {
@@ -26,28 +23,100 @@ public class TransformaImagen {
 
     public void transformaNegativo() throws IOException {
         // Transformar a negativo y guardar como *_n.bmp
-        try (FileInputStream f_in = new FileInputStream(f)) {
-            FileOutputStream f_out = new FileOutputStream(f);
+        try (FileOutputStream f_out = new FileOutputStream(getNameFile() + "_n" + ".bmp")) {
+            FileInputStream f_in = new FileInputStream(f);
             byte[] metaDatos = new byte[54];
-            int byt = f_in.read(metaDatos);
-            while (byt != -1) {
+            f_in.read(metaDatos);
+            f_out.write(metaDatos);
+            int data = 0;
+            while (data != -1) {
+                data = f_in.read();
+                f_out.write(255 - data);
+            }
+        }
+    }
+
+
+    public void transformaOscuro() throws IOException {
+        // Transformar a una imagen más oscura y guardar como *_o.bmp
+        try (FileOutputStream f_out = new FileOutputStream(getNameFile() + "_o" + ".bmp")) {
+            FileInputStream f_in = new FileInputStream(f);
+            byte[] metaDatos = new byte[54];
+            f_in.read(metaDatos);
+            f_out.write(metaDatos);
+            int data = 0;
+            while (data != -1) {
+                data = f_in.read();
+                f_out.write(data / 2);
+            }
+        }
+    }
+
+
+    public void transformaBlancoNegro() throws IOException {
+        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
+        try (FileOutputStream f_out = new FileOutputStream(getNameFile() + "_bn" + ".bmp")) {
+            FileInputStream f_in = new FileInputStream(f);
+            byte[] metaDatos = new byte[54];
+            f_in.read(metaDatos);
+            f_out.write(metaDatos);
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            while (r != -1) {
+                r = f_in.read();
+                g = f_in.read();
+                b = f_in.read();
+                f_out.write((r + g + b) / 3);
+                f_out.write((r + g + b) / 3);
+                f_out.write((r + g + b) / 3);
+            }
+        }
+    }
+
+    public void transformaSepia() throws IOException {
+        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
+        try (FileOutputStream f_out = new FileOutputStream(getNameFile() + "_s" + ".bmp")) {
+            FileInputStream f_in = new FileInputStream(f);
+            byte[] metaDatos = new byte[54];
+            f_in.read(metaDatos);
+            f_out.write(metaDatos);
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            while (r != -1) {
+                r = f_in.read();
+                g = f_in.read();
+                b = f_in.read();
+                r = (int) ((r * 0.393) + (g * 0.769) + (b * 0.189));
+                g = (int) ((r * 0.393) + (g * 0.769) + (b * 0.189));
+                b = (int) ((r * 0.393) + (g * 0.769) + (b * 0.189));
+//                r = (int) ((r + g  + b ));
+//                g = (int) ((r  + g  + b));
+//                b = (int) ((r + g  + b ));
+                if (r > 255) {
+                    f_out.write(r = 255);
+                } else {
+                    f_out.write(r);
+                }
+                if (g > 255) {
+                    f_out.write(g = 255);
+                } else {
+                    f_out.write(g);
+                }
+                if (b > 255) {
+                    f_out.write(b = 255);
+                } else {
+                    f_out.write(b);
+                }
 
             }
         }
     }
 
-    public void transformaOscuro() throws IOException {
-        // Transformar a una imagen más oscura y guardar como *_o.bmp
-
-    }
-
-    public void transformaBlancoNegro() throws IOException {
-        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
-    }
-
-    private String getNombreSinExtension() {
-        //Devuelve el nombre del archivo f sin extensión
-        return "";
+    private String getNameFile() {
+        String partes[] = f.getAbsolutePath().split(".bmp");
+        return partes[0];
     }
 
 }
